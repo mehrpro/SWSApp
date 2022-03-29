d4fb74b13b6465ed79e1cdbcff069957a3875d3a
Farshid Mohammadi <fm708801@gmail.com>
2022-03-29 20:04:05 +0430

    14010109-3

--- SWSApp/UI/MainForm.Designer.cs
+++ SWSApp/UI/MainForm.Designer.cs
@@ -28,12 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mnuInformation = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuAcademicYear = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSchools = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuStudents = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuTagCart = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSMS = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuTaradod = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuReceiver = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuInformation,
+            this.mnuTaradod});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mnuInformation
+            // 
+            this.mnuInformation.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuAcademicYear,
+            this.mnuSchools,
+            this.mnuStudents,
+            this.mnuTagCart,
+            this.mnuSMS});
+            this.mnuInformation.Name = "mnuInformation";
+            this.mnuInformation.Size = new System.Drawing.Size(80, 20);
+            this.mnuInformation.Text = "اطلاعات پایه";
+            // 
+            // mnuAcademicYear
+            // 
+            this.mnuAcademicYear.Name = "mnuAcademicYear";
+            this.mnuAcademicYear.Size = new System.Drawing.Size(180, 22);
+            this.mnuAcademicYear.Text = "سال تحصیلی";
+            // 
+            // mnuSchools
+            // 
+            this.mnuSchools.Name = "mnuSchools";
+            this.mnuSchools.Size = new System.Drawing.Size(180, 22);
+            this.mnuSchools.Text = "مدارس";
+            // 
+            // mnuStudents
+            // 
+            this.mnuStudents.Name = "mnuStudents";
+            this.mnuStudents.Size = new System.Drawing.Size(180, 22);
+            this.mnuStudents.Text = "دانش آموزان";
+            // 
+            // mnuTagCart
+            // 
+            this.mnuTagCart.Name = "mnuTagCart";
+            this.mnuTagCart.Size = new System.Drawing.Size(180, 22);
+            this.mnuTagCart.Text = "تگ کارت";
+            // 
+            // mnuSMS
+            // 
+            this.mnuSMS.Name = "mnuSMS";
+            this.mnuSMS.Size = new System.Drawing.Size(180, 22);
+            this.mnuSMS.Text = "سامانه پیام کوتاه";
+            // 
+            // mnuTaradod
+            // 
+            this.mnuTaradod.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuReceiver});
+            this.mnuTaradod.Name = "mnuTaradod";
+            this.mnuTaradod.Size = new System.Drawing.Size(39, 20);
+            this.mnuTaradod.Text = "تردد";
+            // 
+            // mnuReceiver
+            // 
+            this.mnuReceiver.Name = "mnuReceiver";
+            this.mnuReceiver.Size = new System.Drawing.Size(180, 22);
+            this.mnuReceiver.Text = "تخصیص کارت";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MainForm";
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "ترددبان";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnuInformation;
+        private System.Windows.Forms.ToolStripMenuItem mnuAcademicYear;
+        private System.Windows.Forms.ToolStripMenuItem mnuSchools;
+        private System.Windows.Forms.ToolStripMenuItem mnuStudents;
+        private System.Windows.Forms.ToolStripMenuItem mnuTagCart;
+        private System.Windows.Forms.ToolStripMenuItem mnuSMS;
+        private System.Windows.Forms.ToolStripMenuItem mnuTaradod;
+        private System.Windows.Forms.ToolStripMenuItem mnuReceiver;
     }
 }
